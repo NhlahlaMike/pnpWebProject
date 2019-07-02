b40585ed3141fefd0d333e866419b2c1d93dc56f
Nhlahla_Mike_Mabasa <nhlahlamikemabasa@gmail.com>
2019-07-02 17:02:06 +0200

    Cors installed

---
+++ pnpWebAPI_Project/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace pnpWebAPI_Project.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
