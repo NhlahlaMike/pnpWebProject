786f56e8a226dd1a8fdbcd19c66cac17c267f2a5
Nhlahla_Mike_Mabasa <nhlahlamikemabasa@gmail.com>
2019-07-19 13:33:24 +0200

    Added id property

--- pnpWebAPI_Project/Models/User.cs
+++ pnpWebAPI_Project/Models/User.cs
@@ -14,6 +14,7 @@ namespace pnpWebAPI_Project.Models
     
     public partial class User
     {
+        public int id { get; set; }
         public string Email { get; set; }
         public string Title { get; set; }
         public string FirstName { get; set; }
